47c0b78686654fa945b42f703e52a43c71a52276
Cristhian Richard <richardcristhian94@outlook.com>
2019-05-09 13:03:56 -0300

    Change log files path

--- UI/Program.cs
+++ UI/Program.cs
@@ -23,122 +23,20 @@ namespace DigitalSignage.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            AutoMapperConfig.RegisterMappings();
-            //addTestData();
-
             Log.Logger = new LoggerConfiguration()
-                               .MinimumLevel.Debug()
-                               .WriteTo.File("logs\\Logs.txt", rollingInterval: RollingInterval.Day)
-                               .CreateLogger();
+                   .MinimumLevel.Debug()
+                   .WriteTo.File("C:\\DigitalSignage_Logs\\Logs.txt", rollingInterval: RollingInterval.Day)
+                   .CreateLogger();
+
+            Log.Information("Registering Mappings");
+            AutoMapperConfig.RegisterMappings();
+
 
             Application.Run(new HomeForm());
         }
 
 
-
-
-
-
-
-
-        /// <summary>
-        /// agrega datos de prueba a la DB 
-        /// </summary>
-        static void addTestData()
-        {
-            DigitalSignageDbContext context = new DigitalSignageDbContext();
-            UnitOfWork uow = new UnitOfWork(new DigitalSignageDbContext());
-
-            Campaign campaign = new Campaign()
-            {
-                Name = "Prueba",
-                Description = "Prueba de una campaña",
-                InitialTime = new TimeSpan(0, 0, 1),
-                EndTime = new TimeSpan(23, 30, 0),
-                InitialDate = new DateTime(2018, 02, 07),
-                EndDate = new DateTime(2019, 08, 08),
-                Images = new List<Image>
-      {
-          new Image()
-          {
-              Description = "Imagen 1",
-              Duration = 1,
-              Position = 1,
-              Data = File.ReadAllBytes("../../../assets/images/1.jpg")
-          },
-          new Image()
-          {
-              Description = "Imagen 2",
-              Duration = 2,
-              Position = 2,
-              Data = File.ReadAllBytes("../../../assets/images/2.jpg")
-          },
-          new Image()
-          {
-              Description = "Imagen 3",
-              Duration = 3,
-              Position = 3,
-              Data = File.ReadAllBytes("../../../assets/images/3.jpg")
-          },
-      }
-            };
-            uow.CampaignRepository.Add(campaign);
-
-            uow.Complete();
-
-            RSSSource sourceRSS = new RSSSource()
-            {
-                Description = "Prueba de source",
-                Url = "http://www.bbc.co.uk/mundo/temas/tecnologia/index.xml",
-                RSSItems = new List<RSSItem>
-                {
-                    new RSSItem()
-                    {
-                        Date = DateTime.Now,
-                        Url ="https://www.bbc.com/mundo/internacional/2016/06/160603_sociedad_corea_del_sur_miedo_ventiladores_causa_muerte_ps",
-                        Title = "Por qué en Corea del Sur tanta gente tiene pánico de los ventiladores",
-                        Description = "¿Qué pasa si duermes en una habitación cerrada con un ventilador encendido?",
-                    }
-                }
-            };
-            uow.RSSSourceRepository.Add(sourceRSS);
-            uow.Complete();
-
-            Banner banner1 = new Banner()
-            {
-                Name = "Prueba",
-                Description = "Prueba de banner",
-                InitialTime = new TimeSpan(0, 0, 1),
-                EndTime = new TimeSpan(23, 30, 30),
-                InitialDate = new DateTime(2018, 02, 07),
-                EndDate = new DateTime(2019, 08, 08),
-                Source = sourceRSS,
-                SourceId = sourceRSS.Id
-            };
-            uow.BannerRepository.Add(banner1);
-
-            uow.Complete();
-
-            TextSource textSource = new TextSource()
-            {
-                Data = "Prueba de texto",
-            };
-
-            Banner banner2 = new Banner()
-            {
-                Name = "Prueba",
-                Description = "Prueba de banner",
-                InitialTime = new TimeSpan(0, 0, 1),
-                EndTime = new TimeSpan(23, 30, 30),
-                InitialDate = new DateTime(2018, 02, 07),
-                EndDate = new DateTime(2019, 08, 08),
-                Source = textSource
-            };
-            uow.BannerRepository.Add(banner2);
-        }
     }
-
-
 }
 
 
